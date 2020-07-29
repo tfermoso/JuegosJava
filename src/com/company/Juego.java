4c21689dedc07add795833ef19b6b03564490398
Tomas <tomas.fermoso@gmail.com>
2020-07-29 17:29:29 +0200

    modificadores de acceso y propiedades staticas o de clase

--- src/com/company/Juego.java
+++ src/com/company/Juego.java
@@ -1,23 +1,41 @@
 package com.company;
 
 public class Juego {
-    public int vidas;
+    private int vidas;
+    private int vidasIniciales;
+    private static int record=0;
 
     public Juego(int vidas) {
         this.vidas = vidas;
+        this.vidasIniciales=vidas;
     }
 
     public void MuestravidasRestantes(){
         System.out.println("Número de vidas restantes: "+vidas);
     }
 
-    public static void main(String[] args) {
-        // write your code here
-        Juego juego1=new Juego(5);
-        juego1.MuestravidasRestantes();
-        juego1.vidas--;
-        Juego juego2=new Juego(5);
-        juego1.MuestravidasRestantes();
-        juego2.MuestravidasRestantes();
+    public boolean QuitaVida(){
+        this.vidas--;
+        if(this.vidas==0){
+            System.out.println("Juego Terminado");
+            return false;
+        }else{
+            return true;
+        }
     }
+
+    public void ReiniciaPartida(){
+        this.vidas=this.vidasIniciales;
+    }
+
+    public void ActualizaRecord(){
+        if(this.vidas>Juego.record){
+            Juego.record=this.vidas;
+            System.out.println("Se ha batido el record: "+Juego.record);
+        }else if(this.vidas==Juego.record){
+            System.out.println("Se ha alcanzado el record");
+        }
+    }
+
+
 }
